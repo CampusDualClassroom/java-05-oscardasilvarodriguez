e9fce3a73ec6d8d2406a4ef9de3baaa2a0f3d6e9
oscardasilvarodriguez <grelodj@hotmail.com>
2024-10-29 12:36:35 +0100

    Ejercicio 05, corregido, ya pasa el test

--- src/main/java/com/campusdual/classroom/Exercise05.java
+++ src/main/java/com/campusdual/classroom/Exercise05.java
@@ -4,24 +4,24 @@ public class Exercise05 {
     public static void main(String[] args) {
         checkLessOrGreaterEqual(5, 3);
         checkLessOrGreaterEqual(5, 13);
-        checkMultiply(3, 6);
+        checkMultiply(6, 3);
         checkMultiply(6, 5);
         checkPositiveNegative(6);
         checkPositiveNegative(-6);
     }
 
-        public static void checkLessOrGreaterEqual(int n1, int n2) {
+    public static void checkLessOrGreaterEqual(int n1, int n2) {
         if (n1 < n2) {
             System.out.println(n1 + " es menor que " + n2);
-        }else {
+        } else {
             System.out.println(n1 + " es mayor o igual que " + n2);
         }
     }
 
     public static void checkMultiply(int n1, int n2) {
-        if (n2 % n1 == 0) {
+        if (n1 % n2 == 0) {
             System.out.println(n1 + " es múltiplo de  " + n2);
-        }else {
+        } else {
             System.out.println(n1 + " no es múltiplo de " + n2);
         }
     }
@@ -29,7 +29,7 @@ public class Exercise05 {
     public static void checkPositiveNegative(int num) {
         if (num < 0) {
             System.out.println(num + " es negativo.");
-        }else {
+        } else {
             System.out.println(num + " es positivo.");
         }
     }
